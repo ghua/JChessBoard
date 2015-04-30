5d98bc18bf37e89980b40ff171c1dc810c14e833
Velichko Semyon <semyon@velichko.net>
2015-04-30 17:57:09 +0300

    some refactoring

--- tests/tests.js
+++ tests/tests.js
@@ -12,109 +12,112 @@ QUnit.test("testNumberToCoordinate", function( assert ) {
     assert.deepEqual( _numberToCoordinate(55), [7,6] );
 });
 
-var board = $('canvas');
-board.jschessboard();
+var canvas = $('canvas');
 
-QUnit.test("test _initBoard", function( assert ) {
-    assert.equal(_cells.length, 64);
+QUnit.test("test initBoard", function( assert ) {
+    var board = canvas.jschessboard();
+
+    assert.equal(board.cells.length, 64);
     for (var i = 0; i < 64; i++) {
-        assert.equal(_cells[i], null);
+        assert.equal(board.cells[i], null);
     }
 });
 
 QUnit.test("test _fenToPosition & _positionToFen", function( assert ) {
-    board.startPosition();
+    var board = canvas.jschessboard();
 
     var fenString = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
-    _fenToPosition(board ,  fenString);
-    assert.equal( _positionToFen(board), fenString );
+    board.fenToPosition(fenString);
+    assert.equal( board.positionToFen(), fenString );
 
     fenString = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR';
-    _fenToPosition(board ,  fenString);
-    assert.equal( _positionToFen(board), fenString );
+    board.fenToPosition(fenString);
+    assert.equal( board.positionToFen(), fenString );
 
     fenString = 'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R';
-    _fenToPosition(board ,  fenString);
-    assert.equal( _positionToFen(board), fenString );
+    board.fenToPosition(fenString);
+    assert.equal( board.positionToFen(), fenString );
 });
 
+var jChessPiece = new jChessPiece();
+
 QUnit.test("Pawn isValid test", function( assert ) {
-    assert.ok(_pieces.p.isLegal(0, 2));
-    assert.ok(_pieces.p.isLegal(0, 1));
-    assert.notOk(_pieces.p.isLegal(0, 2)); // second try
-    assert.notOk(_pieces.p.isLegal(0, 3));
-    assert.notOk(_pieces.p.isLegal(1, 1));
-    assert.notOk(_pieces.p.isLegal(2, 0));
-    assert.notOk(_pieces.p.isLegal(0, -1));
+    assert.ok(jChessPiece.types.p.isLegal(0, 2));
+    assert.ok(jChessPiece.types.p.isLegal(0, 1));
+    assert.notOk(jChessPiece.types.p.isLegal(0, 2)); // second try
+    assert.notOk(jChessPiece.types.p.isLegal(0, 3));
+    assert.notOk(jChessPiece.types.p.isLegal(1, 1));
+    assert.notOk(jChessPiece.types.p.isLegal(2, 0));
+    assert.notOk(jChessPiece.types.p.isLegal(0, -1));
 });
 
 QUnit.test("Bishop isValid test", function( assert ) {
-    assert.ok(_pieces.b.isLegal(1, 1));
-    assert.ok(_pieces.b.isLegal(4, 4));
-    assert.ok(_pieces.b.isLegal(-1, -1));
-    assert.ok(_pieces.b.isLegal(-5, -5));
-    assert.ok(_pieces.b.isLegal(-1, 1));
-    assert.notOk(_pieces.b.isLegal(7, 1));
-    assert.notOk(_pieces.b.isLegal(0, 3));
-    assert.notOk(_pieces.b.isLegal(5, -3));
-    assert.notOk(_pieces.b.isLegal(-2, 0));
+    assert.ok(jChessPiece.types.b.isLegal(1, 1));
+    assert.ok(jChessPiece.types.b.isLegal(4, 4));
+    assert.ok(jChessPiece.types.b.isLegal(-1, -1));
+    assert.ok(jChessPiece.types.b.isLegal(-5, -5));
+    assert.ok(jChessPiece.types.b.isLegal(-1, 1));
+    assert.notOk(jChessPiece.types.b.isLegal(7, 1));
+    assert.notOk(jChessPiece.types.b.isLegal(0, 3));
+    assert.notOk(jChessPiece.types.b.isLegal(5, -3));
+    assert.notOk(jChessPiece.types.b.isLegal(-2, 0));
 });
 
 QUnit.test("Knight isValid test", function( assert ) {
     var movesOffsets = [[-1, -2], [-1, 2], [-2, -1], [-2, 1], [1, -2], [1, 2], [2, -1], [2, 1]];
 
     for(var i in movesOffsets) {
-        assert.ok(_pieces.n.isLegal(movesOffsets[i][0], movesOffsets[i][1]));
+        assert.ok(jChessPiece.types.n.isLegal(movesOffsets[i][0], movesOffsets[i][1]));
     }
 
-    assert.notOk(_pieces.n.isLegal(7, 1));
-    assert.notOk(_pieces.n.isLegal(0, 3));
-    assert.notOk(_pieces.n.isLegal(5, -3));
-    assert.notOk(_pieces.n.isLegal(-2, 0));
+    assert.notOk(jChessPiece.types.n.isLegal(7, 1));
+    assert.notOk(jChessPiece.types.n.isLegal(0, 3));
+    assert.notOk(jChessPiece.types.n.isLegal(5, -3));
+    assert.notOk(jChessPiece.types.n.isLegal(-2, 0));
 });
 
 QUnit.test("King isValid test", function( assert ) {
     var movesOffsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, 1]];
 
     for(var i in movesOffsets) {
-        assert.ok(_pieces.k.isLegal(movesOffsets[i][0], movesOffsets[i][1]));
+        assert.ok(jChessPiece.types.k.isLegal(movesOffsets[i][0], movesOffsets[i][1]));
     }
 
-    assert.notOk(_pieces.k.isLegal(2, 1));
-    assert.notOk(_pieces.k.isLegal(7, 1));
-    assert.notOk(_pieces.k.isLegal(0, 3));
-    assert.notOk(_pieces.k.isLegal(5, -3));
-    assert.notOk(_pieces.k.isLegal(-2, 0));
+    assert.notOk(jChessPiece.types.k.isLegal(2, 1));
+    assert.notOk(jChessPiece.types.k.isLegal(7, 1));
+    assert.notOk(jChessPiece.types.k.isLegal(0, 3));
+    assert.notOk(jChessPiece.types.k.isLegal(5, -3));
+    assert.notOk(jChessPiece.types.k.isLegal(-2, 0));
 });
 
 QUnit.test("Rook isValid test", function( assert ) {
-    assert.ok(_pieces.r.isLegal(1, 0));
-    assert.ok(_pieces.r.isLegal(0, 1));
-    assert.ok(_pieces.r.isLegal(5, 0));
-    assert.ok(_pieces.r.isLegal(0, 5));
-    assert.ok(_pieces.r.isLegal(-1, 0));
-    assert.ok(_pieces.r.isLegal(0, -5));
+    assert.ok(jChessPiece.types.r.isLegal(1, 0));
+    assert.ok(jChessPiece.types.r.isLegal(0, 1));
+    assert.ok(jChessPiece.types.r.isLegal(5, 0));
+    assert.ok(jChessPiece.types.r.isLegal(0, 5));
+    assert.ok(jChessPiece.types.r.isLegal(-1, 0));
+    assert.ok(jChessPiece.types.r.isLegal(0, -5));
 
-    assert.notOk(_pieces.r.isLegal(-1, -1));
-    assert.notOk(_pieces.r.isLegal(2, -1));
-    assert.notOk(_pieces.r.isLegal(-5, 5));
+    assert.notOk(jChessPiece.types.r.isLegal(-1, -1));
+    assert.notOk(jChessPiece.types.r.isLegal(2, -1));
+    assert.notOk(jChessPiece.types.r.isLegal(-5, 5));
 });
 
 QUnit.test("Queen isValid test", function( assert ) {
-    assert.ok(_pieces.q.isLegal(1, 0));
-    assert.ok(_pieces.q.isLegal(0, 1));
-    assert.ok(_pieces.q.isLegal(5, 0));
-    assert.ok(_pieces.q.isLegal(0, 5));
-    assert.ok(_pieces.q.isLegal(-1, 0));
-    assert.ok(_pieces.q.isLegal(0, -5));
+    assert.ok(jChessPiece.types.q.isLegal(1, 0));
+    assert.ok(jChessPiece.types.q.isLegal(0, 1));
+    assert.ok(jChessPiece.types.q.isLegal(5, 0));
+    assert.ok(jChessPiece.types.q.isLegal(0, 5));
+    assert.ok(jChessPiece.types.q.isLegal(-1, 0));
+    assert.ok(jChessPiece.types.q.isLegal(0, -5));
 
-    assert.ok(_pieces.q.isLegal(1, 1));
-    assert.ok(_pieces.q.isLegal(4, 4));
-    assert.ok(_pieces.q.isLegal(-1, -1));
-    assert.ok(_pieces.q.isLegal(-5, -5));
-    assert.ok(_pieces.q.isLegal(-1, 1));
+    assert.ok(jChessPiece.types.q.isLegal(1, 1));
+    assert.ok(jChessPiece.types.q.isLegal(4, 4));
+    assert.ok(jChessPiece.types.q.isLegal(-1, -1));
+    assert.ok(jChessPiece.types.q.isLegal(-5, -5));
+    assert.ok(jChessPiece.types.q.isLegal(-1, 1));
 
-    assert.notOk(_pieces.q.isLegal(-5, 4));
-    assert.notOk(_pieces.q.isLegal(2, -1));
-    assert.notOk(_pieces.q.isLegal(6, 5));
+    assert.notOk(jChessPiece.types.q.isLegal(-5, 4));
+    assert.notOk(jChessPiece.types.q.isLegal(2, -1));
+    assert.notOk(jChessPiece.types.q.isLegal(6, 5));
 });
